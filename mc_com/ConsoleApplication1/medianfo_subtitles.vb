dcf41de0c5835c9160a04da9251ac1a4ab58b66d
Playos <Playos@localhost>
2011-11-11 08:45:56 -0800

    Shrunk cache size and lowered loading times for large tv libraries.
    Still dealing with loading issues, should be fixed quickly

---
+++ mc_com/ConsoleApplication1/medianfo_subtitles.vb
@@ -0,0 +1,10 @@
+﻿Imports System
+Imports System.Runtime.InteropServices
+
+Namespace ConsoleApplication1
+    <StructLayout(LayoutKind.Sequential)> _
+    Public Structure medianfo_subtitles
+        Public language As String
+    End Structure
+End Namespace
+
---
+++ mc_com/ConsoleApplication1/profiles.vb
@@ -0,0 +1,13 @@
+﻿Imports System
+Imports System.Collections.Generic
+
+Namespace ConsoleApplication1
+    Public Class profiles
+        ' Fields
+        Public defaultprofile As String
+        Public profilelist As List(Of listofprofiles) = New List(Of listofprofiles)
+        Public startupprofile As String
+        Public workingprofilename As String
+    End Class
+End Namespace
+
