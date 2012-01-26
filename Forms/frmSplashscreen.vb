f7e62f21e3c76ff7d5ebed2af3834c26438229f2
HueyHQ <HueyHQ@localhost>
2012-01-26 20:07:51 +1300

    Just adding some more code intending to centralise all web requests. Only image scraping to go but amalgamating all the code is tedious! Shouldn't break anything but can't be sure as it was a few months ago I did it!

---
+++ Forms/frmSplashscreen.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Net
+Imports System.IO
+Imports System.Data
+Imports System.Text.RegularExpressions
+Imports System.Text
+Imports System.Threading
+Imports Media_Companion.ScraperFunctions
+Imports System.Management
+
+
+Public Class frmSplashscreen
+
+    Dim tempstring As String = ""
+
+    Private Sub splashscreen_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Try
+            'Me.TopMost = True
+            CheckForIllegalCrossThreadCalls = False
+
+            Dim sAssemblyVersion As String = Trim(System.Reflection.Assembly.GetExecutingAssembly.FullName.Split(",")(1))
+            Label2.Text = sAssemblyVersion
+
+
+        Catch ex As Exception
+            ExceptionHandler.LogError(ex)
+        End Try
+    End Sub
+
+End Class
