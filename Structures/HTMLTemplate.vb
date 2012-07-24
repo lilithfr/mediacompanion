55e8bc5a86c90928313c08e7c255745a2f0ff4e0
HueyHQ <HueyHQ@localhost>
2012-07-24 22:43:39 +1200

    Added 'type' to HTMLTemplate structure.

--- Structures/HTMLTemplate.vb
+++ Structures/HTMLTemplate.vb
@@ -2,9 +2,11 @@
     Dim title As String
     Dim path As String
     Dim body As String
+    Dim type As String
     Sub New(SetDefaults As Boolean) 'When called with new keyword & boolean constant SetDefault (either T or F), initialises all values to defaults to avoid having some variables left as 'nothing'
         title = ""
         path = ""
         body = ""
+        type = ""
     End Sub
 End Structure
