8b5e025cbd6ad27ab47d449f2335e6635752e0ac
AnotherPhil <PhilStamp@talktalk.net>
2013-02-02 00:26:49 +0000

    Fix - Stars field is only getting populated with 1 name

---
+++ TvdbScrapper/SeriesList.vb
@@ -0,0 +1,32 @@
+﻿Namespace Tvdb
+    Public Class SeriesList
+        Inherits ProtoXML.ProtoFlatList(Of Series)
+
+        Private Sub New()
+            MyBase.New(Nothing, Nothing)
+            Throw New NotImplementedException()
+        End Sub
+
+        Public Sub New(ByRef Parent As ProtoXML.IProtoXBase, ByVal NodeName As String)
+            MyBase.New(Parent, NodeName)
+        End Sub
+
+
+        Public Overrides Sub ProcessNode(ByRef Element As System.Xml.Linq.XElement)
+
+            Dim NewActor As New Series
+            NewActor.ParentClass = Me.ParentClass
+
+            NewActor.ProcessNode(Element)
+
+            Me.Add(NewActor)
+
+        End Sub
+
+        Public Overrides Function CreateNew() As ProtoXML.IProtoXChild
+            Return New SeriesList
+        End Function
+    End Class
+
+
+End Namespace
