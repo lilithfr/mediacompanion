747250400ea783c245b6a906f4cd3464e07e2d2a
Vbat99 <Vbat99@localhost>
2013-02-03 20:18:18 +1300

    Auto create screenshot working all options.  Find Missing Artworking fixed all options.

---
+++ Classes/ExceptionHandler.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Windows.Forms
+Imports System.Threading
+
+
+Friend NotInheritable Class ExceptionHandler
+    Friend Shared Sub LogError(ByVal ex As Exception)
+        If TypeOf (ex) Is UnauthorizedAccessException Then
+            Dim ErrorMessage As String
+            ErrorMessage = "Unable to write to file.  Please verify file permissions" + vbCrLf + vbCrLf + ex.Message
+            ErrorMessage += vbCrLf + vbCrLf + "Now exiting."
+            MessageBox.Show(ErrorMessage, "Unable to write Metadata", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            For Each frmOpen As Form In My.Application.OpenForms
+                frmOpen.Close()
+            Next
+
+        Else
+            Try
+                Dim ofrmExcept As New frmExceptions
+                ofrmExcept.txtExceptionTrace.Text = ex.ToString
+                ofrmExcept.ShowDialog()
+            Catch ex1 As Exception
+                Dim log As New EventLog
+                Dim source As String = "MediaCompanion"
+                If Not EventLog.SourceExists(source) Then
+                    EventLog.CreateEventSource(source, source)
+                End If
+                log.Source = source
+                log.EnableRaisingEvents = True
+                log.WriteEntry(ex1.ToString, EventLogEntryType.Error)
+                log.WriteEntry(ex.ToString, EventLogEntryType.Error)
+            End Try
+        End If
+    End Sub
+End Class
---
+++ NfoLibrary/Shared/StreamDetails.vb
@@ -0,0 +1,22 @@
+﻿Imports ProtoXML
+
+Public Class StreamDetails
+    Inherits ProtoPropertyGroup
+
+
+    Public Property Video As New VideoDetails(Me, "video")
+    Public Property Audio As New AudioList(Me, "audio")
+    Public Property Subtitles As New SubtitleList(Me, "subtitle")
+
+    Public Sub New()
+        MyBase.New(Nothing, Nothing)
+    End Sub
+
+    Public Sub New(ByRef Parent As IProtoXBase, ByVal NodeName As String)
+        MyBase.New(Parent, NodeName)
+    End Sub
+
+    Public Overrides Function CreateNew() As ProtoXML.ProtoXChildBase
+        Return New StreamDetails
+    End Function
+End Class
