002fb41a78519d4c570d4f67629c6d2d6ecd8d82
AnotherPhil <PhilStamp@talktalk.net>
2013-02-20 00:04:29 +0000

    Latest ffmpeg

---
+++ Scraper dll sources/IMPA/IMPA/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("IMPA")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Media Companion")> 
+<Assembly: AssemblyProduct("IMPA Poster Scraper")> 
+<Assembly: AssemblyCopyright("Copyright © William Adamson 2008")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("2974933d-1f2d-4afb-b509-392d29b7ddb8")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
