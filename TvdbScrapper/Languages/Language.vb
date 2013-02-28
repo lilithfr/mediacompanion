88c221ad50c2b62912adb99796aa5883a9dc8e3f
Vbat99 <Vbat99@localhost>
2013-03-01 11:56:36 +1300

    If ep thumb is available, but 404error, now continues without Index Zero Oops. Fixed in Rescrape.

---
+++ TvdbScrapper/Languages/Language.vb
@@ -0,0 +1,15 @@
+﻿Imports ProtoXML
+
+Namespace Tvdb
+    Public Class Language
+        Inherits ProtoPropertyGroup
+
+        Public Property Language As New ProtoProperty(Me, "name")
+        Public Property Abbreviation As New ProtoProperty(Me, "abbreviation")
+        Public Property Id As New ProtoProperty(Me, "id")
+
+        Public Overrides Function CreateNew() As ProtoXML.IProtoXChild
+            Return New Language
+        End Function
+    End Class
+End Namespace
