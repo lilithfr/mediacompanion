58c6f39d522afd39f6758598747bc1d278099378
Vbat99 <Vbat99@localhost>
2013-03-15 22:33:38 +1300

    Added scan button for missing TV folders in list of separate folders. Linked in TV Refresh All if missing folders are found, option to remove said folders.

---
+++ Classes/ComboList.vb
@@ -0,0 +1,125 @@
+
+Public Class ComboList
+
+    Property fullpathandfilename As String = "" 
+    Property MovieSet            As String = ""
+    Property filename            As String = ""
+    Property foldername          As String = ""
+    Property title               As String = ""
+    Property originaltitle       As String = ""
+    Property year                As String  = ""
+    Property filedate            As String  = ""
+    Property id                  As String  = ""
+    Property rating              As String  = ""
+    Property top250              As String  = 0
+    Property genre               As String  = ""
+    Property playcount           As String  = ""
+    Property sortorder           As String  = ""
+    Property outline             As String  = ""
+    Property runtime             As String  = ""
+    Property createdate          As String  = ""
+    Property missingdata1        As Byte    = 0
+    Property plot                As String  = ""
+    Property source              As String  = ""
+    Property votes               As String  = ""
+    Property Resolution          As Integer = -1
+
+    Public ReadOnly Property MissingFanart As Boolean
+        Get
+            Return _missingdata1 And 1
+        End Get
+    End Property
+
+    Public ReadOnly Property MissingPoster As Boolean
+        Get
+            Return _missingdata1 And 2
+        End Get
+    End Property
+
+    Public ReadOnly Property MissingTrailer As Boolean
+        Get
+            Return _missingdata1 And 4
+        End Get
+    End Property
+
+
+    Public ReadOnly Property MissingRating As Boolean
+        Get
+            Return rating.ToString.Trim=""
+        End Get
+    End Property  
+
+
+    Public ReadOnly Property MissingGenre As Boolean
+        Get
+            Return genre.ToString.Trim=""
+        End Get
+    End Property  
+
+
+    Public ReadOnly Property MissingOutline As Boolean
+        Get
+            Return outline.ToString.Trim=""
+        End Get
+    End Property  
+
+
+    Public ReadOnly Property MissingRuntime As Boolean
+        Get
+            Return runtime=""
+        End Get
+    End Property  
+
+
+    Public ReadOnly Property MissingVotes As Boolean
+        Get
+            Return votes.ToString.Trim=""
+        End Get
+    End Property  
+
+
+    Public ReadOnly Property MissingYear As Boolean
+        Get
+            Return year.ToString.Trim=""
+        End Get
+    End Property  
+
+    Public ReadOnly Property MissingPlot As Boolean
+        Get
+            Return plot.ToString.Trim="" Or plot.ToString.Trim="scraper error"
+        End Get
+    End Property  
+
+    Public ReadOnly Property Watched As Boolean
+        Get
+            Return playcount<>"0"
+        End Get
+    End Property  
+
+
+    Public Sub Assign(From As ComboList)
+        Me.fullpathandfilename = From.fullpathandfilename
+        Me.MovieSet            = From.MovieSet           
+        Me.filename            = From.filename           
+        Me.foldername          = From.foldername         
+        Me.title               = From.title              
+        Me.originaltitle       = From.originaltitle      
+        Me.year                = From.year               
+        Me.filedate            = From.filedate           
+        Me.id                  = From.id                 
+        Me.rating              = From.rating             
+        Me.top250              = From.top250             
+        Me.genre               = From.genre              
+        Me.playcount           = From.playcount          
+        Me.sortorder           = From.sortorder          
+        Me.outline             = From.outline            
+        Me.runtime             = From.runtime            
+        Me.createdate          = From.createdate         
+        Me.missingdata1        = From.missingdata1       
+        Me.plot                = From.plot               
+        Me.source              = From.source             
+        Me.votes               = From.votes              
+        Me.Resolution          = From.Resolution         
+    End Sub
+
+End Class
---
+++ ProtoXML/ProtoGroupList.vb
@@ -0,0 +1,154 @@
+﻿Public Class ProtoGroupList(Of T As IProtoXChild)
+    Inherits ProtoXChildBase
+    Implements IList(Of T)
+
+
+    Public Property ListItemNodeName As String
+
+    Private Blank As ProtoXChildBase
+
+    Public Sub New(ByRef ParentClass As IProtoXBase, ByVal GroupNodeName As String, ByRef ListItemNodeName As String, ByVal Blank As IProtoXChild)
+        MyBase.New(ParentClass, GroupNodeName)
+
+        Me.NodeName = GroupNodeName
+        Me.Node = New XElement(GroupNodeName)
+        Me.ListItemNodeName = ListItemNodeName
+        Me.ParentClass = ParentClass
+        Blank.ParentClass = Me
+        Me.Blank = Blank
+    End Sub
+
+    Protected Sub ProcessAdd(ByVal Item As T)
+        Item.ParentClass = Me
+        If Not Me.Node.Nodes.Contains(Item.Node) Then
+            If Item.Node IsNot Nothing Then
+                If Item.Node.Parent IsNot Nothing Then
+                    Item.Node.Remove()
+                End If
+            End If
+            Me.Node.Add(Item.Node)
+        End If
+        Item.ResolveAttachment(Item)
+
+        List.Add(Item)
+    End Sub
+
+    Public List As New List(Of T)
+
+    Sub Add(ByVal item As T) Implements ICollection(Of T).Add
+        item.ParentClass = Me
+        If Not Me.Node.Nodes.Contains(item.Node) Then
+            If item.Node IsNot Nothing Then
+                If item.Node.Parent IsNot Nothing Then
+                    item.Node.Remove()
+                End If
+            End If
+            Me.Node.Add(item.Node)
+        End If
+        item.ResolveAttachment(item)
+
+        List.Add(item)
+    End Sub
+
+    Public Sub Clear() Implements ICollection(Of T).Clear
+        For Each Node As XElement In (From X As XElement In ParentClass.Node.Nodes Where X.Name = Me.NodeName)
+            Node.Remove()
+        Next
+
+        List.Clear()
+    End Sub
+
+    Public Function Contains(ByVal item As T) As Boolean Implements ICollection(Of T).Contains
+        Return List.Contains(item)
+    End Function
+
+    Public Sub CopyTo(ByVal array() As T, ByVal arrayIndex As Integer) Implements ICollection(Of T).CopyTo
+        List.CopyTo(array)
+    End Sub
+
+    Public ReadOnly Property Count As Integer Implements ICollection(Of T).Count
+        Get
+            Return List.Count
+        End Get
+    End Property
+
+    Public ReadOnly Property IsReadOnly As Boolean Implements ICollection(Of T).IsReadOnly
+        Get
+            Return False
+        End Get
+    End Property
+
+    Public Function Remove(ByVal item As T) As Boolean Implements ICollection(Of T).Remove
+        If List.Contains(item) Then
+            item.Node.Remove()
+            List.Remove(item)
+
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Public Function GetEnumerator() As IEnumerator(Of T) Implements IEnumerable(Of T).GetEnumerator
+        Return List.GetEnumerator
+    End Function
+
+    Public Function IndexOf(ByVal item As T) As Integer Implements IList(Of T).IndexOf
+        If item Is Nothing Then Return Nothing
+
+        Return List.IndexOf(item)
+    End Function
+
+    Public Sub Insert(ByVal index As Integer, ByVal item As T) Implements IList(Of T).Insert
+        List.Item(index).Node.AddBeforeSelf(item.Node)
+        List.Insert(index, item)
+    End Sub
+
+    Default Public Property Item(ByVal index As Integer) As T Implements IList(Of T).Item
+        Get
+            If List.Count > index Then
+                Return List.Item(index)
+            Else
+                Return Nothing
+            End If
+        End Get
+        Set(ByVal value As T)
+            List.Item(index) = value
+        End Set
+    End Property
+
+    Public Sub RemoveAt(ByVal index As Integer) Implements IList(Of T).RemoveAt
+        List.Item(index).Node.Remove()
+        List.RemoveAt(index)
+    End Sub
+
+    Public Function GetEnumerator1() As IEnumerator Implements IEnumerable.GetEnumerator
+        Return List.GetEnumerator()
+    End Function
+
+    Public Overrides Function CreateNew() As IProtoXChild
+        Return New ProtoGroupList(Of T)(Me.ParentClass, Me.NodeName, Me.ListItemNodeName, Me.Blank)
+    End Function
+
+    Public Overrides Sub ProcessNode(ByRef Element As XElement)
+        Me.Node = Element
+
+        Dim ChildProperty As IProtoXChild
+        For Each Child As XElement In Me.Node.Nodes
+
+            If Child.Name.ToString.ToLower = Me.ListItemNodeName.ToLower Then
+                Dim NewItem As IProtoXChild = Me.Blank.CreateNew()
+                NewItem.ParentClass = Me
+                NewItem.ProcessNode(Child)
+                List.Add(NewItem)
+            ElseIf Me.ChildrenLookup.ContainsKey(Child.Name.ToString.ToLower) Then
+                ChildProperty = Me.ChildrenLookup.Item(Child.Name.ToString.ToLower)
+
+                ChildProperty.ProcessNode(Child)
+            End If
+        Next
+    End Sub
+
+End Class
+
+
