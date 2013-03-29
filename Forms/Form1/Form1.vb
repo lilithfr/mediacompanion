db1744e56dd256cdef5f77183cf90e5ce58a7a3b
AnotherPhil <PhilStamp@talktalk.net>
2013-03-29 15:33:56 +0000

    Fixed: Showing movie filter configure menu option

--- Forms/Form1/Form1.vb
+++ Forms/Form1/Form1.vb
@@ -10,17 +10,9 @@ Imports System.Reflection
 Imports System.Windows.Forms
 Imports System.ComponentModel
 Imports System.Linq
-'Imports System.Runtime.InteropServices
 
 #Const SilentErrorScream = False
 #Const NoRefocus = True
-
-'Module ModWindows
-'    <DllImport("user32.dll")> _
-'    Function WindowFromPoint(ByVal Point As POINT) As IntPtr
-'    End Function
-'End Module
-
 
 
 Public Class Form1
@@ -22805,10 +22797,13 @@ Public Class Form1
         ApplyMovieFilters
     End Sub
 
-    Property ShowMovieFilterConfig As Boolean = True
+
+    Private Sub cbFilterBeginSliding(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cbFilterYear.BeginSliding, cbFilterVotes.BeginSliding, cbFilterRating.BeginSliding
+        SplitContainer5.Panel2.ContextMenuStrip = Nothing
+    End Sub
 
     Private Sub cbFilterEndSliding(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cbFilterYear.EndSliding, cbFilterVotes.EndSliding, cbFilterRating.EndSliding
-        ShowMovieFilterConfig = False
+        SplitContainer5.Panel2.ContextMenuStrip = cmsConfigureMovieFilters
     End Sub
 
     Private Sub ApplyMovieFilters
@@ -24095,31 +24090,6 @@ End Sub
     Private Sub tsmiCheckForNewVersion_Click( sender As System.Object,  e As System.EventArgs) Handles tsmiCheckForNewVersion.Click
         BckWrkCheckNewVersion.RunWorkerAsync(True)
     End Sub
-
-
-    Private Sub cmsConfigureMovieFilters_Opening(sender As Object,  e As CancelEventArgs) Handles cmsConfigureMovieFilters.Opening
-
-        If Not ShowMovieFilterConfig Then
-            ShowMovieFilterConfig = True
-            e.Cancel = True
-        End If
-
-        'Dim c As Control = GetControlAtMousePosition
-
-        'If IsNothing(c) Then Return
-
-        'If GetControlAtMousePosition.GetType = GetType(UserControl_RangeSlider.SelectionRangeSlider) Then e.Cancel = True
-    End Sub
-
-
-    'Function GetControlAtMousePosition As Control
-
-    '    Dim hWnd As IntPtr = WindowFromPoint(Control.MousePosition)
-
-    '    If hWnd = IntPtr.Zero Then Return Nothing
-
-    '    Return Control.FromHandle(hWnd)
-    'End Function
 
 
 End Class
