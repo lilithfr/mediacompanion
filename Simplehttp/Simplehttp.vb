6f89be006e1b1100a61a06d8ee72dd7492cc3a85
vbat99 <vbat99@localhost>
2014-01-08 17:08:01 +1300

    Best add the files in correct place and to project.

---
+++ Simplehttp/Simplehttp.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Threading
+Imports System.Net
+Imports System.IO
+Imports System.Text.RegularExpressions
+Imports System.Xml
+Imports System.IO.Compression
+Imports System.Text
+Imports System.Web
+Imports System.Reflection
+Imports System.Security.Cryptography
+Imports Microsoft.Win32
+
+Public Class Simplehttp
+
+End Class
