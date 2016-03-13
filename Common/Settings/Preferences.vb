84e318d8f16fd9c19ccd8da6b414f790d71e738c
AnotherPhil <AnotherPhil@localhost>
2016-03-13 17:44:29 +0000

    Bug fix: Some DTSHD-MA audio tracks being incorrectly recognised as DTS

--- Common/Settings/Preferences.vb
+++ Common/Settings/Preferences.vb
@@ -2521,7 +2521,7 @@ Public Class Pref
                     If audio.Codec.Value.ToLower.IndexOf("truehd")>-1 Then audio.Codec.Value = "truehd"
                     tmpaud = aviFile.Audio(curAS).FormatID.ToLower()
                     If audio.Codec.Value = "DTS" Then
-                        If tmpaud.ToLower = "dts ma / core" Then
+                        If tmpaud.ToLower.indexof("dts ma")>-1 Then ' tmpaud.ToLower = "dts ma / core" Then
                             audio.Codec.Value = "dtshd_ma"
                         ElseIf tmpaud.ToLower = "dts hra / core" Then
                             audio.Codec.Value = "dtshd_hra"
