e2f4cc9535d238d0fd39afee430f3b49cb634f04
vbat99 <vbat99@localhost>
2017-09-08 15:32:21 +1200

    MC 3.678b

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("3.6.7.7")> 
-<Assembly: AssemblyFileVersion("3.6.7.7")> 
+<Assembly: AssemblyVersion("3.6.7.8")> 
+<Assembly: AssemblyFileVersion("3.6.7.8")> 
